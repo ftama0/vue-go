5762fa1c04a7b641191475348938b35bfdc4be32
Ferry Pratama <fryatama@gmail.com>
2024-11-25 09:38:29 +0800

    test passing props

--- src/pages/absen/face.vue
+++ src/pages/absen/face.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const data = route.query.data
 </script>
 
 <template>
     <main>
         <h1>hello face</h1>
+        <p>Data: {{ data }}</p>
     </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,11 +6,16 @@ const router = useRouter()
 function goToAbsen() {
     router.push('/absen')
 }
+
+function goToFace() {
+    router.push({ path: '/absen/face', query: { data: 'exampleData' } })
+}
 </script>
 
 <template>
     <main>
         <h1>hello index</h1>
         <button @click="goToAbsen">Go to Absen</button>
+        <button @click="goToFace">Go to Face with Data</button>
     </main>
 </template>
